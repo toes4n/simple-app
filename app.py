a9710339823a3d5fef098a6a6d5396d3e4486a94
toesan <67867354+Toe-San@users.noreply.github.com>
2025-05-19 18:23:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Toe San Testing + GitHub Actions!'
+    return 'Hello, Toe San Testing Deploy GitHub Actions!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
