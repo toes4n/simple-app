984641801f5ae4ca25566da7b82d247925a9ebed
toesan <67867354+Toe-San@users.noreply.github.com>
2025-05-19 18:42:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+# In your app.py
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Toe San Testing Deploy Success with GitHub Actions!'
+    return 'Hello, Docker + GitHub Actions!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)  # Important: host='0.0.0.0'
