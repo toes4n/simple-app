06ff2108722531652f036431807d0ab8a4bc83c4
toesan <67867354+Toe-San@users.noreply.github.com>
2025-05-28 15:41:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello Toe San, Flask App Successfully Deployed.'
+    return 'Hello Toe San, Flask App Successfully Deployed update 1.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # Important: host='0.0.0.0'
